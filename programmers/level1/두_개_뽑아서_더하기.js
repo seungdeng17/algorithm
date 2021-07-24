f25568ad20bb136f1daadde3d515ab9ecdea16a1
sgdg <seungdeng17@gmail.com>
2021-07-24 19:59:15 +0900

    solution

---
+++ programmers/level1/두_개_뽑아서_더하기.js
@@ -0,0 +1,13 @@
+// https://programmers.co.kr/learn/courses/30/lessons/68644
+
+function solution(numbers) {
+  var answer = new Set();
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.add(numbers[i] + numbers[j]);
+    }
+  }
+
+  return [...answer].sort((a, b) => a - b);
+}
