7ff779eca8e5415098e422030d7e5f1df18fb7d9
이승택 <seungdeng17@gmail.com>
2021-08-03 10:45:45 +0900

    solution

---
+++ baekjoon/1806_부분합.js
@@ -0,0 +1,19 @@
+function solution(m, arr) {
+  let answer = Number.MAX_SAFE_INTEGER;
+
+  let lt = 0;
+  let rt = 0;
+  while (rt < arr.length) {
+    let sum = 0;
+    for (let i = lt; i <= rt; i++) sum += arr[i];
+
+    if (sum >= m) {
+      answer = Math.min(answer, rt - lt + 1);
+      lt++;
+    } else rt++;
+  }
+
+  return answer;
+}
+
+console.log(solution(15, [5, 1, 3, 5, 10, 7, 4, 9, 2, 8]));
