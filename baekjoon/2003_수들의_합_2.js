d6728cef1e9a35951e7bdd1b650c44984f61de3f
이승택 <seungdeng17@gmail.com>
2021-08-03 10:34:44 +0900

    solution

---
+++ baekjoon/2003_수들의_합_2.js
@@ -0,0 +1,21 @@
+function solution(m, arr) {
+  let answer = 0;
+
+  let lt = 0;
+  let rt = 0;
+  while (rt < arr.length) {
+    let sum = 0;
+    for (let i = lt; i <= rt; i++) sum += arr[i];
+
+    if (sum < m) rt++;
+    else if (sum > m) lt++;
+    else {
+      answer++;
+      rt++;
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution(5, [1, 2, 3, 4, 2, 5, 3, 1, 1, 2]));
