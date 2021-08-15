ad3202eb2a971262c4e2a5e0eb647b997433ed5e
sgdg <seungdeng17@gmail.com>
2021-08-15 16:48:05 +0900

    solution

--- inflearn/자바스크립트 알고리즘 문제풀이/섹션1/08_일곱_난쟁이.js
+++ inflearn/자바스크립트 알고리즘 문제풀이/섹션1/08_일곱_난쟁이.js
@@ -1,14 +1,25 @@
+// function solution(arr) {
+//   const sum = arr.reduce((acc, n) => acc + n);
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (sum - (arr[i] + arr[j]) === 100) {
+//         arr.splice(i, 1);
+//         arr.splice(j - 1, 1);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
 function solution(arr) {
-  const sum = arr.reduce((acc, n) => acc + n);
-  for (let i = 0; i < arr.length - 1; i++) {
+  const total = arr.reduce((a, b) => a + b);
+  for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (sum - (arr[i] + arr[j]) === 100) {
-        arr.splice(i, 1);
-        arr.splice(j - 1, 1);
+      if (total - (arr[i] + arr[j]) === 100) {
+        return arr.filter((_, index) => !(index === i || index === j));
       }
     }
   }
-  return arr;
 }
 
 console.log(solution([20, 7, 23, 19, 10, 15, 8, 13, 25]));
--- inflearn/자바스크립트 알고리즘 문제풀이/섹션2/05_등수구하기.js
+++ inflearn/자바스크립트 알고리즘 문제풀이/섹션2/05_등수구하기.js
@@ -1,8 +1,23 @@
+// function solution(arr) {
+//   return arr
+//     .slice()
+//     .sort((a, b) => b - a)
+//     .map((n) => arr.indexOf(n) + 1);
+// }
+
 function solution(arr) {
-  return arr
-    .slice()
-    .sort((a, b) => b - a)
-    .map((n) => arr.indexOf(n) + 1);
+  const answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let r = 1;
+    for (let j = 0; j < arr.length; j++) {
+      if (i === j) continue;
+      if (arr[i] < arr[j]) r++;
+    }
+    answer.push(r);
+  }
+
+  return answer;
 }
 
 console.log(solution([87, 89, 92, 100, 76]));
