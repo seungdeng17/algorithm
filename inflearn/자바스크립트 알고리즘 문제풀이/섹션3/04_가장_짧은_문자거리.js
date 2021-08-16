f8d55341798cd46324c9bdfac3371d8c02402d0c
sgdg <seungdeng17@gmail.com>
2021-08-16 19:05:05 +0900

    solution

--- inflearn/자바스크립트 알고리즘 문제풀이/섹션3/04_가장_짧은_문자거리.js
+++ inflearn/자바스크립트 알고리즘 문제풀이/섹션3/04_가장_짧은_문자거리.js
@@ -1,27 +1,47 @@
+// function solution(s, t) {
+//   const answer = [];
+
+//   let p = 100;
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === t) {
+//       p = 0;
+//     } else {
+//       p++;
+//     }
+//     answer.push(p);
+//   }
+
+//   p = 100;
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     if (s[i] === t) {
+//       p = 0;
+//     } else {
+//       p++;
+//     }
+
+//     if (answer[i] > p) {
+//       answer.splice(i, 1, p);
+//     }
+//   }
+
+//   return answer;
+// }
+
 function solution(s, t) {
   const answer = [];
 
-  let p = 100;
+  let p = s.length;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] === t) {
-      p = 0;
-    } else {
-      p++;
-    }
+    if (s[i] === t) p = 0;
+    else p++;
     answer.push(p);
   }
 
-  p = 100;
+  p = s.length;
   for (let i = s.length - 1; i >= 0; i--) {
-    if (s[i] === t) {
-      p = 0;
-    } else {
-      p++;
-    }
-
-    if (answer[i] > p) {
-      answer.splice(i, 1, p);
-    }
+    if (s[i] === t) p = 0;
+    else p++;
+    answer[i] = Math.min(answer[i], p);
   }
 
   return answer;
