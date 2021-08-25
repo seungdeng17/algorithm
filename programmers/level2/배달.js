bcf6ea01478c28cac0c2923f34708a49d19f3276
이승택 <seungdeng17@gmail.com>
2021-08-25 18:11:21 +0900

    solution

--- programmers/level2/배달.js
+++ programmers/level2/배달.js
@@ -1,12 +1,36 @@
 // https://programmers.co.kr/learn/courses/30/lessons/12978
-// 풀이중
+
+// function solution(N, road, K) {
+//   const graph = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
+//   for (let [start, end, score] of road) {
+//     if (graph[start][end] && score > graph[start][end]) continue;
+//     graph[start][end] = score;
+//     graph[end][start] = score;
+//   }
+
+//   const ch = [];
+//   const set = new Set();
+//   function DFS(node, sum) {
+//     if (sum > K) return;
+//     if (sum <= K) set.add(node);
+//     for (let i = 1; i < graph[0].length; i++) {
+//       if (i === node || ch[i] || !graph[node][i]) continue;
+//       ch[i] = 1;
+//       DFS(i, sum + graph[node][i]);
+//       ch[i] = 0;
+//     }
+//   }
+//   ch[1] = 1;
+//   DFS(1, 0);
+
+//   return set.size;
+// }
 
 function solution(N, road, K) {
-  const graph = Array.from({ length: N + 1 }, () => Array(N + 1).fill(0));
+  const graph = Array.from({ length: N + 1 }, () => []);
   for (let [start, end, score] of road) {
-    if (graph[start][end] && score > graph[start][end]) continue;
-    graph[start][end] = score;
-    graph[end][start] = score;
+    graph[start].push([end, score]);
+    graph[end].push([start, score]);
   }
 
   const ch = [];
@@ -14,11 +38,12 @@ function solution(N, road, K) {
   function DFS(node, sum) {
     if (sum > K) return;
     if (sum <= K) set.add(node);
-    for (let i = 1; i < graph[0].length; i++) {
-      if (i === node || ch[i] || !graph[node][i]) continue;
-      ch[i] = 1;
-      DFS(i, sum + graph[node][i]);
-      ch[i] = 0;
+    for (let i = 0; i < graph[node].length; i++) {
+      const [end, score] = graph[node][i];
+      if (end === node || ch[end]) continue;
+      ch[end] = 1;
+      DFS(end, sum + score);
+      ch[end] = 0;
     }
   }
   ch[1] = 1;
@@ -27,20 +52,20 @@ function solution(N, road, K) {
   return set.size;
 }
 
-// console.log(
-//   solution(
-//     5,
-//     [
-//       [1, 2, 1],
-//       [2, 3, 3],
-//       [5, 2, 2],
-//       [1, 4, 2],
-//       [5, 3, 1],
-//       [5, 4, 2],
-//     ],
-//     3
-//   )
-// );
+console.log(
+  solution(
+    5,
+    [
+      [1, 2, 1],
+      [2, 3, 3],
+      [5, 2, 2],
+      [1, 4, 2],
+      [5, 3, 1],
+      [5, 4, 2],
+    ],
+    3
+  )
+);
 console.log(
   solution(
     6,
@@ -56,12 +81,3 @@ console.log(
     4
   )
 );
-
-[
-  [0, 1, 2, 0, 0, 0],
-  [1, 0, 2, 0, 0, 0],
-  [2, 2, 0, 3, 2, 0],
-  [0, 0, 3, 0, 0, 0],
-  [0, 0, 2, 0, 0, 1],
-  [0, 0, 0, 0, 1, 0],
-];
