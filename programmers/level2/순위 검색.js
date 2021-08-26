77504c93450104affe9baa2b347e48e55066372d
sgdg <seungdeng17@gmail.com>
2021-08-26 20:52:34 +0900

    solution

--- programmers/level2/순위 검색.js
+++ programmers/level2/순위 검색.js
@@ -1,36 +1,84 @@
 // https://programmers.co.kr/learn/courses/30/lessons/72412
 // 풀이중
 
+// function solution(info, query) {
+//   const answer = [];
+
+//   query.forEach((cmd, idx) => {
+//     let cnt = 0;
+//     const score = +cmd.match(/([\d]+)$/)[0];
+//     cmd = cmd.replace(score, "").trim();
+//     const tmp = cmd.split(" and ");
+
+//     const ch = Array.from({ length: info.length }, () => 1);
+//     for (let s of tmp) {
+//       if (s === "-") continue;
+//       for (let i = 0; i < info.length; i++) {
+//         if (!ch[i]) continue;
+//         if (!info[i].includes(s)) {
+//           ch[i] = 0;
+//           continue;
+//         }
+//       }
+//     }
+//     for (let i = 0; i < info.length; i++) {
+//       if (!ch[i]) continue;
+//       if (+info[i].match(/([\d]+)$/)[0] < score) ch[i] = 0;
+//     }
+//     for (let c of ch) if (c) cnt++;
+
+//     answer[idx] = cnt;
+//   });
+
+//   return answer;
+// }
+
 function solution(info, query) {
   const answer = [];
 
-  query.forEach((cmd, idx) => {
-    let cnt = 0;
-    const score = +cmd.match(/([\d]+)$/)[0];
-    cmd = cmd.replace(score, "").trim();
-    const tmp = cmd.split(" and ");
-
-    const ch = Array.from({ length: info.length }, () => 1);
-    for (let s of tmp) {
-      if (s === "-") continue;
-      for (let i = 0; i < info.length; i++) {
-        if (!ch[i]) continue;
-        if (!info[i].includes(s)) {
-          ch[i] = 0;
-          continue;
-        }
-      }
+  const map = new Map();
+  const subset = getSubset(4);
+  for (let data of info) {
+    const score = +data.match(/([\d]+)$/)[0];
+    data = data.replace(score, "").trim();
+    const tmp = data.split(" ");
+    for (let set of subset) {
+      const _tmp = tmp.slice();
+      for (let i = 0; i < set.length; i++) _tmp[set[i]] = "-";
+      const str = _tmp.join("");
+      if (map.has(str)) map.get(str).push(score);
+      else map.set(str, [score]);
     }
-    for (let i = 0; i < info.length; i++) {
-      if (!ch[i]) continue;
-      if (+info[i].match(/([\d]+)$/)[0] < score) ch[i] = 0;
-    }
-    for (let c of ch) if (c) cnt++;
+  }
 
-    answer[idx] = cnt;
-  });
+  for (let i = 0; i < query.length; i++) {
+    const score = +query[i].match(/([\d]+)$/)[0];
+    const _query = query[i].replace(score, "").replace(/and|\s/g, "");
+    if (!map.has(_query)) {
+      answer[i] = 0;
+      continue;
+    }
+    answer[i] = map.get(_query).filter((n) => n >= score).length;
+  }
 
   return answer;
+}
+
+function getSubset(n) {
+  const subset = [];
+  const tmp = [];
+  function DFS(L) {
+    if (L === n) {
+      subset.push([...tmp]);
+      return;
+    }
+    tmp.push(L);
+    DFS(L + 1);
+    tmp.pop();
+    DFS(L + 1);
+  }
+  DFS(0);
+  return subset;
 }
 
 console.log(
