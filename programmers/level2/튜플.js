5958a9b841289b84d04ad802bd5379ca22097c6f
이승택 <seungdeng17@gmail.com>
2021-09-14 12:00:06 +0900

    solution

--- programmers/level2/튜플.js
+++ programmers/level2/튜플.js
@@ -1,18 +1,20 @@
 // https://programmers.co.kr/learn/courses/30/lessons/64065
 
 function solution(s) {
-  return s
-    .match(/\{[\d|\,]+\}/g) // 중괄호 단위로 분리
-    .map((s) => s.replace(/\{|\}/g, "").split(","))
-    .sort((a, b) => a.length - b.length) // 원소 개수가 낮은 순으로 정렬
-    .reduce((acc, tuple) => {
-      for (const t of tuple) {
-        if (acc.includes(+t)) continue;
-        acc.push(+t); // 자리수에 해당하는 값 푸시
-        break;
-      }
-      return acc;
-    }, []);
+  return [
+    ...s
+      .match(/\{[\d|\,]+\}/g) // 중괄호 단위로 분리
+      .map((s) => s.replace(/\{|\}/g, "").split(","))
+      .sort((a, b) => a.length - b.length) // 원소 개수가 낮은 순으로 정렬
+      .reduce((set, tuple) => {
+        for (const t of tuple) {
+          if (set.has(+t)) continue;
+          set.add(+t); // 자리수에 해당하는 값 푸시
+          break;
+        }
+        return set;
+      }, new Set()),
+  ];
 }
 
 console.log(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
