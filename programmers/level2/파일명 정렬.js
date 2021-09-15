68910e5255ccd54c7939541ad13305e1467fe9e6
sgdg <seungdeng17@gmail.com>
2021-09-15 13:27:10 +0900

    solution

--- programmers/level2/파일명 정렬.js
+++ programmers/level2/파일명 정렬.js
@@ -4,7 +4,7 @@ function solution(arr) {
   return arr.sort((a, b) => {
     let [aHead, aNumber] = a
       .split(/([^0-9]+)([0-9]{1,5})(.*)/)
-      .filter((s) => s);
+      .filter((s) => s); // Head, Number 분리
     let [bHead, bNumber] = b
       .split(/([^0-9]+)([0-9]{1,5})(.*)/)
       .filter((s) => s);
@@ -13,11 +13,11 @@ function solution(arr) {
     bHead = bHead.toLowerCase();
 
     if (aHead !== bHead) {
-      const [_aHead, _bHead] = [aHead, bHead].sort();
-      return aHead === _aHead ? -1 : 1;
+      const [_aHead, _bHead] = [aHead, bHead].sort(); // Head가 같은 문자열이 아니면 사전순 정렬
+      return aHead === _aHead ? -1 : 1; // 사전순으로 정렬했을 때 기존 정렬 상태와 같으면 스왑
     }
-    if (+aNumber === +bNumber) return 0;
-    return +aNumber - +bNumber;
+    if (+aNumber === +bNumber) return 0; // Number가 같으면 정렬 상태 유지
+    return +aNumber - +bNumber; // 다르면 오름차순 정렬
   });
 }
 
