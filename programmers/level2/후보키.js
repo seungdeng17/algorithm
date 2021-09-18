9706b935244727bef938e5b09e2c40b007a5c53c
sgdg <seungdeng17@gmail.com>
2021-09-18 14:44:31 +0900

    solution

--- programmers/level2/후보키.js
+++ programmers/level2/후보키.js
@@ -1,7 +1,42 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42890
 // 풀이중
 
-function solution() {}
+function solution(relation) {
+  const answer = [];
+  const len = relation.length;
+
+  const totalSubset = getSubset(relation[0].length);
+  for (const subset of totalSubset) {
+    if (answer.some((k) => subset.join("").includes(k))) continue;
+
+    const set = new Set();
+    for (const data of relation) {
+      let tmp = "";
+      for (const idx of subset) tmp += data[idx];
+      set.add(tmp);
+    }
+    if (set.size === len) answer.push(subset.join(""));
+  }
+
+  return answer.length;
+}
+
+function getSubset(n) {
+  const set = [];
+  const tmp = [];
+  function DFS(L) {
+    if (L === n) {
+      if (!tmp.length) return;
+      return set.push([...tmp]);
+    }
+    tmp.push(L);
+    DFS(L + 1);
+    tmp.pop();
+    DFS(L + 1);
+  }
+  DFS(0);
+  return set.sort((a, b) => a.length - b.length);
+}
 
 console.log(
   solution([
