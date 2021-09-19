d709771eb5b2c5ed005cb915b6b18087f2d9332d
sgdg <seungdeng17@gmail.com>
2021-09-19 18:31:25 +0900

    solution

--- programmers/level2/빛의 경로 사이클.js
+++ programmers/level2/빛의 경로 사이클.js
@@ -7,19 +7,20 @@ function solution(grid) {
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
 
-  const board = Array.from({ length: grid.length }, (_, i) =>
-    grid[i].split("")
-  );
   const ch = Array.from({ length: grid.length }, () => []);
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[0].length; j++) ch[i].push([0, 0, 0, 0]);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) ch[i].push([0, 0, 0, 0]);
   }
 
-  for (let d = 0; d < dx.length; d++) {
-    const cnt = DFS(0, 0, d, 0);
-    if (cnt) answer.push(cnt);
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      for (let d = 0; d < dx.length; d++) {
+        if (ch[i][j][d]) continue;
+        const cnt = DFS(i, j, d, 0);
+        if (cnt) answer.push(cnt);
+      }
+    }
   }
-  // console.log(DFS(0, 0, 1, 0));
 
   function DFS(x, y, d, cnt) {
     if (ch[x][y][d]) return cnt;
@@ -27,13 +28,12 @@ function solution(grid) {
 
     let nx = x + dx[d];
     let ny = y + dy[d];
-    if (nx < 0) nx = board.length - 1;
-    if (nx >= board.length) nx = 0;
-    if (ny < 0) ny = board[0].length - 1;
-    if (ny >= board[0].length) ny = 0;
+    if (nx < 0) nx = grid.length - 1;
+    if (nx >= grid.length) nx = 0;
+    if (ny < 0) ny = grid[0].length - 1;
+    if (ny >= grid[0].length) ny = 0;
 
-    const nd = getNextDir(board[nx][ny], d);
-    // console.log(`(${x}, ${y}) -> (${nx}, ${ny}) / ${d}`);
+    const nd = getNextDir(grid[nx][ny], d);
     return DFS(nx, ny, nd, cnt + 1);
   }
 
