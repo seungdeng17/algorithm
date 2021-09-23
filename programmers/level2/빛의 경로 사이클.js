67371a0dc7b891f2b684b7faa53ece38f92f89f8
sgdg <seungdeng17@gmail.com>
2021-09-23 13:35:22 +0900

    solution

--- programmers/level2/빛의 경로 사이클.js
+++ programmers/level2/빛의 경로 사이클.js
@@ -3,11 +3,13 @@
 function solution(grid) {
   const answer = [];
 
+  // 빛의 다음 방향 좌표 계산을 위한 데이터
   const dx = [-1, 1, 0, 0];
   const dy = [0, 0, -1, 1];
 
+  // 방문한 방향을 체크할 체크 배열
   const ch = Array.from({ length: grid.length }, () => []).map((c) => {
-    for (let i = 0; i < grid[0].length; i++) c.push([0, 0, 0, 0]);
+    for (let i = 0; i < grid[0].length; i++) c.push([0, 0, 0, 0]); // 상하좌우
     return c;
   });
 
@@ -24,7 +26,7 @@ function solution(grid) {
   function checker(x, y, d) {
     let cnt = 0;
     while (true) {
-      if (ch[x][y][d]) break;
+      if (ch[x][y][d]) break; // 이미 방문한 방향일 때 break
       ch[x][y][d] = 1;
       cnt++;
 
@@ -43,6 +45,7 @@ function solution(grid) {
 }
 
 function getNextDir(block, dir) {
+  // 0상, 1하, 2좌, 3우
   if (block === "S") return dir;
   if (block === "L") return [2, 3, 1, 0][dir];
   if (block === "R") return [3, 2, 0, 1][dir];
