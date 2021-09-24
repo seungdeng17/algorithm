aaec83e7b111a5dfa0809f2bfb3af6add0196def
sgdg <seungdeng17@gmail.com>
2021-09-24 12:45:53 +0900

    solution

--- leetcode/110. Balanced Binary Tree.js
+++ leetcode/110. Balanced Binary Tree.js
@@ -2,11 +2,11 @@ function isBalanced(root) {
   if (!root) return true;
 
   function getDepth(node, depth) {
-    if (!node) return depth;
+    if (!node) return depth; // 하위 노드 없으면 계산된 깊이 리턴
     const ld = getDepth(node.left, depth + 1);
     const rd = getDepth(node.right, depth + 1);
-    if (Math.abs(ld - rd) > 1) return -1;
-    return Math.max(ld, rd);
+    if (Math.abs(ld - rd) > 1) return -1; // 좌우 노드 깊이 균형이 1 초과시 false 조건 리턴
+    return Math.max(ld, rd); // 노드 깊이 리턴
   }
 
   return getDepth(root, 0) !== -1;
