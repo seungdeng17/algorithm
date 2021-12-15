5e9eebcf88f1c9d2fcaf46a2f319b68726af3189
이승택 <seungdeng17@gmail.com>
2021-12-15 19:25:51 +0900

    solution

---
+++ goorm/점심은 햄버거.js
@@ -0,0 +1,28 @@
+const input = [];
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on('line', function (line) {
+  input.push(line)
+}).on('close', function () {
+	console.log(solution(input));
+  process.exit();
+});
+
+function solution(input) {
+	const result = [];
+	let [cnt, eT, bT] = input;
+	eT = eT.split(' ').sort((a, b) => b - a);
+	bT = bT.split(' ').sort((a, b) => a - b);
+	
+	let tmp = 0;
+	for (let i = 0; i < cnt; i++) {
+		tmp += +bT[i];
+		result.push(tmp + +eT[i]);
+	}
+	
+	return Math.max(...result);
+}
