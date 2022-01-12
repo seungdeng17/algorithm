2705793ad1fc6aa242a8419ffc77f5a1369436ec
sgdg <seungdeng17@gmail.com>
2022-01-12 21:15:38 +0900

    solution

--- inflearn/자바스크립트 알고리즘 문제풀이/섹션01/08_일곱_난쟁이.js
+++ inflearn/자바스크립트 알고리즘 문제풀이/섹션01/08_일곱_난쟁이.js
@@ -1,22 +1,34 @@
+// // function solution(arr) {
+// //   const sum = arr.reduce((acc, n) => acc + n);
+// //   for (let i = 0; i < arr.length - 1; i++) {
+// //     for (let j = i + 1; j < arr.length; j++) {
+// //       if (sum - (arr[i] + arr[j]) === 100) {
+// //         arr.splice(i, 1);
+// //         arr.splice(j - 1, 1);
+// //       }
+// //     }
+// //   }
+// //   return arr;
+// // }
+
 // function solution(arr) {
-//   const sum = arr.reduce((acc, n) => acc + n);
-//   for (let i = 0; i < arr.length - 1; i++) {
+//   const total = arr.reduce((a, b) => a + b);
+//   for (let i = 0; i < arr.length; i++) {
 //     for (let j = i + 1; j < arr.length; j++) {
-//       if (sum - (arr[i] + arr[j]) === 100) {
-//         arr.splice(i, 1);
-//         arr.splice(j - 1, 1);
+//       if (total - (arr[i] + arr[j]) === 100) {
+//         return arr.filter((_, index) => !(index === i || index === j));
 //       }
 //     }
 //   }
-//   return arr;
 // }
 
 function solution(arr) {
-  const total = arr.reduce((a, b) => a + b);
-  for (let i = 0; i < arr.length; i++) {
+  const sum = arr.reduce((a, b) => a + b, 0);
+
+  for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      if (total - (arr[i] + arr[j]) === 100) {
-        return arr.filter((_, index) => !(index === i || index === j));
+      if (sum - (arr[i] + arr[j]) === 100) {
+        return arr.filter((_, index) => index !== i && index !== j);
       }
     }
   }
